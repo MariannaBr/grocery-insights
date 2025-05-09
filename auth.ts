9d0a0c5e10333bc17904956dfe52d6bb3b8387f4
Marianna Brezinova <brezinova.marianna@gmail.com>
2025-05-09 13:42:14 -0700

    fixed error

--- auth.ts
+++ auth.ts
@@ -10,7 +10,7 @@ export const authOptions = {
       credentials: {
         email: { label: "Email", type: "email" },
         name: { label: "Name", type: "name" },
-        password: { label: "Password", type: "password" },
+        password: { label: "Password", type: "password" }
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
@@ -18,7 +18,7 @@ export const authOptions = {
         }
 
         const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
+          where: { email: credentials.email }
         });
 
         if (!user) {
@@ -26,14 +26,36 @@ export const authOptions = {
             data: {
               name: credentials.name ?? credentials.email,
               email: credentials.email,
-              password: await bcrypt.hash(credentials.password, 10),
+              accounts: {
+                create: {
+                  type: "credentials",
+                  provider: "credentials",
+                  providerAccountId: credentials.email,
+                  password: await bcrypt.hash(credentials.password, 10)
+                }
+              }
             },
+            include: {
+              accounts: true
+            }
           });
+        }
+
+        const account = await prisma.account.findFirst({
+          where: {
+            userId: user.id,
+            type: "credentials",
+            provider: "credentials"
+          }
+        });
+
+        if (!account?.password) {
+          throw new Error("Invalid credentials");
         }
 
         const isCorrectPassword = await bcrypt.compare(
           credentials.password,
-          user.password
+          account.password
         );
 
         if (!isCorrectPassword) {
@@ -41,11 +63,11 @@ export const authOptions = {
         }
 
         return user;
-      },
-    }),
+      }
+    })
   ],
   pages: {
-    signIn: "/login",
+    signIn: "/login"
   },
   callbacks: {
     async jwt({ token, user }) {
@@ -53,6 +75,6 @@ export const authOptions = {
     },
     async session({ session, token }) {
       return { ...session, user: { ...session.user, id: token.id } };
-    },
-  },
+    }
+  }
 } satisfies NextAuthOptions;
